99fcbad4fc373b38a7d0215dc566536e92ea1ad6
LuKey <63052673+lukeyleung21@users.noreply.github.com>
2022-03-25 21:28:12 +0800

    goDown_Finish

--- Lift/src/Lift_Component/goDown.java
+++ Lift/src/Lift_Component/goDown.java
@@ -1,24 +1,19 @@
-package Button;
+package Lift.src.Lift_Component;
 import java.util.ArrayList;
-import Lift.Lift_1;
-import Door.*;
 public class goDown extends Door{
     public void goDown(int now_level,ArrayList<Integer> Stop_list){
         int x = now_level;
+        Door door = new Door();
         while(x!=0){
-            for(int temp=0;temp<Stop_list.size();temp++)
+            for(int temp=0;temp<Stop_list.size();temp++)            //condition of Stop_list
             {
                 if(x == Stop_list.get(temp)){
-                    System.out.println(x);
+                    door.Door=door.setDoor();
+                                                        //在這里設開門時間
+                    door.Door=door.setDoor();
                 }
             }
             x-=1;
         }
-    }
-    public boolean setDoor(boolean door){
-        if(door==true){
-            return false;
-        }
-        else{return true;}
     }
 }
