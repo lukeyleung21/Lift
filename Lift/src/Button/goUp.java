565d451f0ba18bede9e29e502bd5fba70c646cc3
LuKey <63052673+lukeyleung21@users.noreply.github.com>
2022-03-25 16:35:33 +0800

    Error of coding

--- Lift/src/Button/goUp.java
+++ Lift/src/Button/goUp.java
@@ -1,4 +1,9 @@
 package Button;
 public class goUp{
-    
+    public void goUp(int now_level){
+        int x = now_level;
+        while(x!=0){
+            x-=1;
+        }
+    }
 }
--- Lift/src/Lift/Lift_1.java
+++ Lift/src/Lift/Lift_1.java
@@ -1,3 +1,16 @@
 package Lift;
+import java.util.ArrayList;
+
+import Button.*;
+import Level.*;
 public class Lift_1{
+    public static boolean Door = false;
+    public static void main(String[] args){
+        goDown x = new goDown();
+        Level_Control c = new Level_Control();
+        c.set_Stop_level(15);
+        x.goDown(c.get_now_level(),c.get_Stop_level());
+        
+        
+    }
 }
