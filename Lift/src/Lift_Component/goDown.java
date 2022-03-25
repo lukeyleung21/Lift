921a1b14104a510d2ee563a87ecc5039c11316f7
LuKey <63052673+lukeyleung21@users.noreply.github.com>
2022-03-26 00:21:35 +0800

    Optimized.

--- Lift/src/Lift_Component/goDown.java
+++ Lift/src/Lift_Component/goDown.java
@@ -1,19 +1,24 @@
 package Lift.src.Lift_Component;
 import java.util.ArrayList;
 public class goDown extends Door{
-    public void goDown(int now_level,ArrayList<Integer> Stop_list){
-        int x = now_level;
+    public void goDown(Level_Control Level_info){
+        int now_level = Level_info.get_now_level();
+        int Lower = Level_info.get_lowest_stopList();
         Door door = new Door();
-        while(x!=0){
-            for(int temp=0;temp<Stop_list.size();temp++)            //condition of Stop_list
+        while(now_level>=Lower){
+            System.out.println(now_level);
+            for(int temp=0;temp<Level_info.get_Stop_level().size();temp++)            //condition of Stop_list
             {
-                if(x == Stop_list.get(temp)){
+                
+                if(now_level == Level_info.get_Stop_level().get(temp)){
+                    System.out.println("Stop");
                     door.Door=door.setDoor();
                                                         //在這里設開門時間
                     door.Door=door.setDoor();
                 }
             }
-            x-=1;
+            now_level-=1;
         }
+        now_level+=1;
     }
 }
