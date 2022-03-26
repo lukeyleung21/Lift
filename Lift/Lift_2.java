036cc16d4322f60ffb28ce47e420c98d716fb538
LuKey <63052673+lukeyleung21@users.noreply.github.com>
2022-03-26 11:10:52 +0800

    Lift_2.java

---
+++ Lift/Lift_2.java
@@ -0,0 +1,12 @@
+import Lift_Component.*;
+import Lift_Component.Error_Handle.ExcceedError;
+public class Lift_2 {
+    public static void main(String[] args)throws Exception{
+        Go x = new Go();
+        Level_Control c = new Level_Control();
+        c.set_Stop_level(10);
+        c.set_Stop_level(0);
+        x.goDown(c);
+        x.goUp(c);
+}
+}
