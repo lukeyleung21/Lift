2bc9d3c7d88fbdad8a7874b2f7470f8d0234b3e4
LuKey <63052673+lukeyleung21@users.noreply.github.com>
2022-04-02 00:38:06 +0800

    Try it

--- Lift/Lift.java
+++ Lift/Lift.java
@@ -1,10 +1,12 @@
 import Lift_1_Component.*;
 import Lift_2_Component.*;
 public class Lift{
-   
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
     Lift_1 lift1 = new Lift_1();
     Lift_2 lift2 = new Lift_2();
-        
+        lift1.set_Stop_level(10);
+        lift2.set_Stop_level(5);
+        lift1.Go(true);
+        lift2.Go(true);
     }
 }
--- Lift/Lift_2_Component/Lift_2.java
+++ Lift/Lift_2_Component/Lift_2.java
@@ -1,4 +1,15 @@
 package Lift_2_Component;
 public class Lift_2{
-    private int now_level = 0;
+    public void set_Stop_level(int x) throws Exception{
+        Level_Control a = new Level_Control();
+        
+        a.set_Stop_level(x);
+    }
+    public void Go(boolean x) throws Exception{
+        Go go = new Go();
+            if(x == true){          //true = up  false == down
+                go.goUp();
+            }
+            else{go.goDown();}
+        }
 }
