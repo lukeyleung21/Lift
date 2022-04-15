a30d1d4f5eb6469de10a746591d6d0848b4d64b5
LuKey <63052673+lukeyleung21@users.noreply.github.com>
2022-04-15 14:40:33 +0800

    Delete Lift directory

--- Lift/Component/Control.java
+++
@@ -1,8 +0,0 @@
-package Component;
-import java.util.ArrayList;
-interface Control{
-    final int top_floor=9;
-    public static ArrayList<Integer> stop_list= new ArrayList<Integer>();
-    public void goUp() throws InterruptedException;
-    public void goDown() throws InterruptedException;
-}
--- Lift/LiftInput.java
+++
@@ -1,5 +0,0 @@
-import Component.InLift;
-interface LiftInput{
-    InLift L1 = new Lift();
-    InLift L2 = new Lift();
-}
